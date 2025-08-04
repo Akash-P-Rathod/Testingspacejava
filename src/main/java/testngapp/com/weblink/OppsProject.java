df75c9794933faec86a634d6c2977fe754093227
Akash-P-Rathod <85490215+Akash-P-Rathod@users.noreply.github.com>
2025-08-04 15:15:47 +0530

    Update OppsProject.java
    Akash

--- src/main/java/testngapp/com/weblink/OppsProject.java
+++ src/main/java/testngapp/com/weblink/OppsProject.java
@@ -5,6 +5,11 @@ public class OppsProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello Akash");
+		webdriver driver;
+		System.setproperty("wedriver.chrome.driver","C:\Users\INTEL\eclipse-workspace-program\com.weblink\drivers\chromedriver.exe")
+			Webdriver driver=New ChromerDriver();
+		driver.get("htttps://www.weblinkservices.net");
+			
 	}
 
 }
