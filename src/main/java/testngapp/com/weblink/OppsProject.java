38ec6dedb10c9b183f7dde9deba6eb9c29aa6d21
INTEL <INTEL@DESKTOP-725HS8N>
2025-08-04 15:20:29 +0530

    this is updated

--- src/main/java/testngapp/com/weblink/OppsProject.java
+++ src/main/java/testngapp/com/weblink/OppsProject.java
@@ -1,14 +1,17 @@
 package testngapp.com.weblink;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class OppsProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello Akash");
-		webdriver driver;
-		System.setproperty("wedriver.chrome.driver","C:\Users\INTEL\eclipse-workspace-program\com.weblink\drivers\chromedriver.exe")
-			Webdriver driver=New ChromerDriver();
-		driver.get("htttps://www.weblinkservices.net");
+		WebDriver driver;
+		System.setProperty("wedriver.chrome.driver","C:\\Users\\INTEL\\eclipse-workspace-program\\com.weblink\\drivers\\chromedriver.exe");
+			WebDriver driver1= new ChromeDriver();
+		driver1.get("https://www.weblinkservices.net");
 			
 	}
 
